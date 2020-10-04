028459e0c7f3866a14c4ce0733988d7d5a7ed581
Christian Lacaste <34718381+cjlacaste28@users.noreply.github.com>
2020-10-05 06:31:24 +0800

    Update script2.js

--- assets/js/script2.js
+++ assets/js/script2.js
@@ -3,7 +3,7 @@ let adapter = new LocalStorage("GoalsDB");
 let GoalsDB = low(adapter);
 //db init
 GoalsDB.defaults({ users: [], goals: [], currLogin: [] }).write();
-const baseurl = window.location.origin;
+const baseurl = window.location;
 console.log(baseurl);
 /********************* INITIALIZATION ***********************/
 const signUpButton = document.getElementById('signUp');
